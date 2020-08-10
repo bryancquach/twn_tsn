722e8fdde6182ea5869b84a0a1cd16d5693fb088
Bryan Quach <bryancquach@users.noreply.github.com>
2020-08-10 13:14:58 -0400

    Update recover_TSN.R
    Added data type defaults for argparser arguments. Fixed logical error in matrix subsetting of covariates

--- tsn/recover_TSN.R
+++ tsn/recover_TSN.R
@@ -1,4 +1,3 @@
-
 #This recovers tissue-specific networks for tissues from Bicmix output
 ##### Chuan Gao, edited and updated by A. Gewirtz #####
 
@@ -10,24 +9,24 @@ library(argparser)
 
 # argument parsing
 args <- arg_parser("program", hide.opts=T);
-args <- add_argument(args, "--out", help="output directory")
+args <- add_argument(args, "--out", help="output directory", type = "character")
 args <- add_argument(args, "--gn", help="file with gene names in order used in
-                                     expression matrix, one per line")
+                                     expression matrix, one per line", type = "character")
 args <- add_argument(args, "--cov", help="covariate matrix with samples as columns
-                                    and covariates as rows")
-args <- add_argument(args, "--cn", help="covariate names")
+                                    and covariates as rows", type = "character")
+args <- add_argument(args, "--cn", help="covariate names", type = "character")
 args <- add_argument(args, "--rd", help="directory with bicmix run outputs(each
-                                     as a directory)")
-args <- add_argument(args, "--it", help="iteration at which to use bicmix 
-                                      output")
-args <- add_argument(args, "--nr", help="number of runs of bicmix that you ran")
-args <- add_argument(args, "--gn_prob", help="GeneNet probability that an edge is 
-                                      non-zero")
+                                     as a directory)", type = "character")
+args <- add_argument(args, "--it", help="iteration at which to use bicmix
+                                      output", type = "character")
+args <- add_argument(args, "--nr", help="number of runs of bicmix that you ran", type = "integer")
+args <- add_argument(args, "--gn_prob", help="GeneNet probability that an edge is
+                                      non-zero", type = "numeric")
 args <- add_argument(args, "--thresh", help="number of times you require an edge
-                                       be duplicated to be included in network 
+                                       be duplicated to be included in network
                                        = ceiling ( number of runs a covariate-
                                        specific factor was found in / THRESH)",
-                                        default=4)
+                                        default=4, type = "numeric")
 
 argv     <- parse_args(args)
 cov_fn   <- argv$cov
@@ -63,7 +62,7 @@ read.data <- function(dir, sd, itr, nS, nG, nCovTotal){
   }else if(file.info(paste(dir, sd, "/LAM_", itr, sep = ""))$size == 0){
     return("noContent")
   }else{
-    lamCov <- t(matrix(scan(paste(dir, sd, "/LAM_", itr, sep = "")), 
+    lamCov <- t(matrix(scan(paste(dir, sd, "/LAM_", itr, sep = "")),
                 ncol = nG))
     lam <- lamCov[1:nG,]
     z <- t(matrix(scan(paste(dir, sd, "/Z_", itr, sep = "")), ncol = 2))
@@ -77,11 +76,11 @@ read.data <- function(dir, sd, itr, nS, nG, nCovTotal){
 }
 
 
-# function to identify covariate specific factors: returns two list corresponding to 
-#the two levels of the covariate; each list contains the index of the factors that 
-#are specific to that level, and a matrix containing the number of samples that are 
-#non-zeros for that factor level in the first row, and the number of samples that are 
-#non-zeros for the other factor level in the second row 
+# function to identify covariate specific factors: returns two list corresponding to
+#the two levels of the covariate; each list contains the index of the factors that
+#are specific to that level, and a matrix containing the number of samples that are
+#non-zeros for that factor level in the first row, and the number of samples that are
+#non-zeros for the other factor level in the second row
 
 find.fac.spec <- function(data, covSpec){
   lam <- data$lam; ex <- data$ex; z <- data$z; o <- data$o
@@ -123,18 +122,18 @@ get.network.all.component.spec <- function(data, facSpec, gn_prob){
     return(0)
   }
 
-  LXXL <- lam[indexGene,facSpec] %*% VXX %*% t(lam[indexGene, facSpec]) + 
+  LXXL <- lam[indexGene,facSpec] %*% VXX %*% t(lam[indexGene, facSpec]) +
             diag(PSI[indexGene])
   LXXLi <- solve(LXXL)
   posrows <- which(diag(LXXL) > 0)
   precision <- cov2cor(LXXLi[posrows, posrows])
   nanrows <- which(is.na(precision))
   if(length(nanrows) > 1){
-	 precision <- precision[-(nanrows),]
+         precision <- precision[-(nanrows),]
   }
 
   exitStatus <- tryCatch({
-    arth.edges <- network.test.edges(precision, direct = FALSE, plot = FALSE)     
+    arth.edges <- network.test.edges(precision, direct = FALSE, plot = FALSE)
     if(nrow(precision) == 2){
       arth.edges <- t(arth.edges)
       arth.net <- arth.edges[arth.edges[,6] > gn_prob,]
@@ -142,7 +141,7 @@ get.network.all.component.spec <- function(data, facSpec, gn_prob){
       arth.net <- arth.edges[arth.edges[,6] > gn_prob,]
     }
   },error = function(e){return("Error")})
-  
+
   if(length(arth.net) == 0){
     return(all.net="no edges")
   }
@@ -154,37 +153,37 @@ get.network.all.component.spec <- function(data, facSpec, gn_prob){
   return(arth.net)
 }
 
-# vote on the duplicated edges using the ensemble method. 
-# nDup is the number of times each edge is replicated. 
+# vote on the duplicated edges using the ensemble method.
+# nDup is the number of times each edge is replicated.
 vote.network<- function(all.net, geneNames, nDup){
   all.net.out <- list()
   all.net.bak <- all.net
-  
+
   edges <- data.frame(seed = all.net$sd, edges = paste(
             all.net$node1, "_", all.net$node2, sep = ""), stringsAsFactors = F)
   edgesND <- edges[!duplicated(edges$edges),]
   rownames(edgesND) <- edgesND$edges
   Dtable <- table(edges[,1:2])
-  
+
   countDupEdges <- apply(Dtable, 2, function(x){return(sum(x > 0))})
   dupSeed <- apply(Dtable, 2, function(x){return(paste(names(which(x != 0)),
               collapse = ","))})
-  
+
   Dindex <- (countDupEdges > nDup)
   if(sum(Dindex) == 0){return("noEdges")}
   goodEdges <- data.frame(edgesND[names(Dindex)[Dindex],], countEdges =
-                countDupEdges[Dindex], DupSeed = dupSeed[Dindex], 
+                countDupEdges[Dindex], DupSeed = dupSeed[Dindex],
                 stringsAsFactors = F)
-  
+
   n <- nrow(goodEdges)
   node1 <- sapply(goodEdges[,2], function(x){return(strsplit(x, "_")[[1]][1])})
   node2 <- sapply(goodEdges[,2], function(x){return(strsplit(x, "_")[[1]][2])})
-  all.net.out$edges <- data.frame(Source = node1, Target = node2, 
+  all.net.out$edges <- data.frame(Source = node1, Target = node2,
                         Weight = goodEdges[,3])
-  
+
   nodeAll <- sort(as.numeric(as.character(c(node1, node2))))
   nodeAll <- nodeAll[!duplicated(nodeAll)]
-  
+
   AllSd <- sapply(nodeAll, function(x){
     index <- grep(x, goodEdges[,2])
     AllSdi <- lapply(goodEdges$DupSeed[index], function(x){
@@ -194,8 +193,8 @@ vote.network<- function(all.net, geneNames, nDup){
     AllSdi <- paste(AllSdi, collapse = ",")
     return(AllSdi)
   })
-  
-  nodeFull <- data.frame(Id = nodeAll, Label = geneNames[nodeAll], DupSeed = 
+
+  nodeFull <- data.frame(Id = nodeAll, Label = geneNames[nodeAll], DupSeed =
                 AllSd, stringsAsFactors = F)
   all.net.out$nodes <- nodeFull
   return(all.net.out)
@@ -213,7 +212,7 @@ seeds2use <- c(1:num_runs)
 
 # find the tissue-specific factors from each run
 for(i in 1:length(seeds2use)){
-  isd <- seeds2use[i]   
+  isd <- seeds2use[i]
   data <- read.data(dir, isd, itr, nS, nG, nCovTotal);
   lam <- data$lam; ex <- data$ex; z <- data$z; o <- data$o; EXX <- data$EXX
   PSI <- data$PSI
@@ -222,7 +221,7 @@ for(i in 1:length(seeds2use)){
 
   # iTis below controls which covariates you find tissue-specific networks for
   for(iTis in c(1:nCovTotal)){
-    facSpec <- find.fac.spec(data, cov[,iTis]);
+    facSpec <- find.fac.spec(data, cov[iTis,]);
     if(length(facSpec[[1]]$whichFac) == 0){
       next
     }
@@ -231,12 +230,12 @@ for(i in 1:length(seeds2use)){
     if(length(arth.net) == 1){
       next
     }
-    all.net[[iTis]] <- rbind(all.net[[iTis]], data.frame(arth.net, 
+    all.net[[iTis]] <- rbind(all.net[[iTis]], data.frame(arth.net,
                         sd = rep(isd, nrow(arth.net))))
 
     sdFac[[iTis]] <- rbind(sdFac[[iTis]], data.frame(
-                      sd = rep(isd, length(facSpec[[1]]$whichFac)), 
-                      fac = facSpec[[1]]$whichFac, 
+                      sd = rep(isd, length(facSpec[[1]]$whichFac)),
+                      fac = facSpec[[1]]$whichFac,
                       lamCount = lamCount[facSpec[[1]]$whichFac],
                       xCount = xCount[facSpec[[1]]$whichFac]))
   }
@@ -245,25 +244,25 @@ for(i in 1:length(seeds2use)){
 # select final edges to be in each covariate specific network
 for(iTis in c(1:nCovTotal)){
   # get the number of runs of bicmix that produced a covariate-specific factor
-  sdTisi <- all.net[[iTis]][,7] 
+  sdTisi <- all.net[[iTis]][,7]
   sdTisi <- sdTisi[!duplicated(sdTisi)]
   nSd <- length(sdTisi)
   if(nSd == 0){
     next
   }
   # number of duplications required for an edge to be included
-  nDup <- ceiling(nSd / thresh) 
+  nDup <- ceiling(nSd / thresh)
 
   voteNetwork <- vote.network(all.net[[iTis]], geneNames, nDup)
   if(length(voteNetwork) == 1){
     next
   }
 
-  write.table(voteNetwork$nodes[,1:2], paste(outdir, covNames[iTis], 
+  write.table(voteNetwork$nodes[,1:2], paste(outdir, covNames[iTis],
     "_spec_ensemble_nDup", nDup,"_nodes.csv", sep = ""), sep=";", quote = F,
     row.names = F, col.names = names(voteNetwork$nodes[,1:2]))
 
-  write.table(voteNetwork$edges, paste(outdir, covNames[iTis], 
+  write.table(voteNetwork$edges, paste(outdir, covNames[iTis],
     "_spec_ensemble_nDup", nDup, "_edges.csv", sep = ""), sep = ";", quote = F,
     row.names = F, col.names = names(voteNetwork$edges))
 }
